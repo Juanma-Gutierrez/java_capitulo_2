b287368e55f236cec6703429800a9393957e2758
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-06 13:58:32 +0200

    feat: Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,18 @@
+/*
+* Exercise: 3
+* Created: 06/10/2022 13:54
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        String nombre = "Juan Manuel Gutiérrez";
+        String direccion = "C/ Frederick Termann, s/n - PTA Malaga - Campanillas - Malaga";
+        String telefono = "952.11.22.33";
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(telefono);
+
+    }
+}
