b1c1d5c84d8cbda034d7e7092cbf9105478979c1
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-06 14:26:34 +0200

    feat: Ejercicio8

---
+++ Ejercicio8.java
@@ -0,0 +1,24 @@
+/*
+* Exercise: 8
+* Created: 06/10/2022 14:22
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        char a;
+        char b;
+        char c;
+        char d;
+        char e;
+        String cadena;
+        a = 'a';
+        b = 'b';
+        c = 'c';
+        d = 'd';
+        e = 'e';
+        cadena = "" + a + b + c + d + e;
+        System.out.print(cadena);
+    }
+}
