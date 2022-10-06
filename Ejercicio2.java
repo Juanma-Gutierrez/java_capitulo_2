6fdc5981f3c802d52cb01ef22aa1296dfa121741
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-06 13:53:55 +0200

    feat: Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,14 @@
+/*
+* Exercise: 2
+* Created: 06/10/2022 13:48
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String nombre;
+        nombre = "Juanma Gutiérrez";
+        System.out.print(nombre);
+    }
+}
