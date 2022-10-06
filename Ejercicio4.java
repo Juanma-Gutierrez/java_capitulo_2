57e95320c6feacfd59eb28261eca3e137addbe18
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-06 14:04:57 +0200

    feat: Ejercicio4

---
+++ Ejercicio4.java
@@ -0,0 +1,17 @@
+/*
+* Exercise: 4
+* Created: 06/10/2022 13:58
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        float euros;
+        float conver;
+        conver = 166.386f;
+        euros = 1000f;
+        System.out.print(euros + " euros son: " + (int) (euros * conver) + " pesetas");
+    }
+
+}
