43982c0408215e5fcba3fe37ec2cae688cecdece
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-06 14:14:28 +0200

    feat: Ejercicio6

---
+++ Ejercicio6.java
@@ -0,0 +1,21 @@
+/*
+* Exercise: 6
+* Created: 06/10/2022 14:09
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        char a;
+        char b;
+        String s1;
+        String s2;
+        a = 'a';
+        b = 'b';
+        s1 = "String 1";
+        s2 = "String 2";
+        System.out.println(a + "" + b + s1 + s2);
+    }
+}
